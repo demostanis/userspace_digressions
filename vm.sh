ef5bf7f84bab8e4f6366a898c918e743df062e35
demostanis <demostanis@protonmail.com>
2024-06-18 23:26:22 +0200

    use minimal alpine image

--- vm.sh
+++ vm.sh
@@ -6,7 +6,7 @@ msg() {
 	echo "$@" >&2
 }
 
-iso_url=https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-standard-3.20.0-x86_64.iso
+iso_url=https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-virt-3.20.1-x86_64.iso
 iso_name=alpine.iso
 
 # download an alpine image file
@@ -22,11 +22,11 @@ trap 'rm -rf "$tmp"' EXIT
 
 # replace /init with ours
 msg unpacking initramfs to replace /init...
-zcat boot/initramfs-lts | cpio --quiet -idD "$tmp"
+zcat boot/initramfs-virt | cpio --quiet -idD "$tmp"
 install -Dm755 ./init "$tmp"/init
 ( cd "$tmp"; find -print0 | sort -z | \
 	cpio -o0 --quiet -H newc | gzip
-) > boot/initramfs-lts
+) > boot/initramfs-virt
 
 # boot it without passing through grub
 msg booting...
@@ -34,8 +34,8 @@ qemu-system-x86_64 \
 	-enable-kvm \
 	-m 512M \
 	-smp cores=2 \
-	-kernel boot/vmlinuz-lts \
-	-initrd boot/initramfs-lts \
+	-kernel boot/vmlinuz-virt \
+	-initrd boot/initramfs-virt \
 	-drive file="$iso_name",format=raw \
 	-append "console=ttyS0" \
 	-nographic
