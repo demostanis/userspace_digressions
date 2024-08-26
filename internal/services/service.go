658ed44a81dc76dda4948ea4db1b1aba8f86dc68
DinoMalin <jcario@student.42lehavre.fr>
2024-08-26 12:50:07 +0200

    mutex to avoid kernel panics

--- internal/services/service.go
+++ internal/services/service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"os"
 	"os/exec"
+	"sync"
 )
 
 type Runlevel int
@@ -31,13 +32,17 @@ func IsEnabled(service string) bool {
 	return err == nil
 }
 
+var mu sync.Mutex
+
 func (service *Service) Run() {
 	// hack because I'm too lazy to add another property to the service parsing
 	commandShouldOutputToSystemLog := service.Name != "syslogd"
 
 	cmd := exec.Command("/bin/sh", "-c", service.Command)
 
+	mu.Lock()
 	OnOffMap[service.Name] = "ON"
+	mu.Unlock()
 	if commandShouldOutputToSystemLog {
 		log := exec.Command("/usr/bin/logger", "-t", service.Name)
 
@@ -56,7 +61,9 @@ func (service *Service) Run() {
 	} else {
 		cmd.Run()
 	}
+	mu.Lock()
 	OnOffMap[service.Name] = "OFF"
+	mu.Unlock()
 }
 
 func StartServices() {
@@ -65,7 +72,9 @@ func StartServices() {
 		runlevel := <-RunlevelChan
 		for _, service := range services[runlevel] {
 			if !IsEnabled(service.Name) {
+				mu.Lock()
 				OnOffMap[service.Name] = "OFF"
+				mu.Unlock()
 				continue
 			}
 			go service.Run()
